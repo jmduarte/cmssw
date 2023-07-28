1816ec3d5acb6de2b7d25758f5599cec834c65b3
Javier Duarte <jduarte@ucsd.edu>
2023-07-28 23:45:31 +0200

    reorder

--- L1Trigger/L1TGlobal/src/TriggerMenu.cc
+++ L1Trigger/L1TGlobal/src/TriggerMenu.cc
@@ -62,8 +62,8 @@ TriggerMenu::TriggerMenu(
       m_vecMuonShowerTemplate(vecMuonShowerTemplateVal),
       m_vecCaloTemplate(vecCaloTemplateVal),
       m_vecEnergySumTemplate(vecEnergySumTemplateVal),
-      m_vecAXOL1TLTemplate(vecAXOL1TLTemplateVal),  //new
-      m_vecExternalTemplate(vecExternalTemplateVal),
+      m_vecExternalTemplate(vecExternalTemplateVal)l),
+      m_vecm_vecAXOL1TLTemplate(vecAXOL1TLTemplateVal)w
       m_vecCorrelationTemplate(vecCorrelationTemplateVal),
       m_vecCorrelationThreeBodyTemplate(vecCorrelationThreeBodyTemplateVal),
       m_vecCorrelationWithOverlapRemovalTemplate(vecCorrelationWithOverlapRemovalTemplateVal),
